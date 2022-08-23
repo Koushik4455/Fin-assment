7fd3103eae17af0d20a0b1c2c5a8e998d8677ab8
koushik <koushikgaddam4455@gmail.com>
2022-08-23 10:10:13 +0530

    fin assment

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const Login = () => {
   const [data,setData] = useState({
@@ -10,14 +10,21 @@ const Login = () => {
   const changeHandle =(e)=>{
     setData({...data,[e.target.name]:e.target.value})
   }
-  const { id } = useEffect ()
+  const { id } = useParams ()
   useEffect(()=>{
-    Login ()
+    login ()
   },[])
+  const login =async () =>{
+    const res= await axios.get(`https://ring-ring-food.herokuapp.com/api/user/${id}`)
+    setData(res.data)
+
+  }
+
   const submit = (e) =>{
     e.preventDefault ()
-    axios.put(` https://ring-ring-food.herokuapp.com/api/user/${id}`)
+    axios.put(`https://ring-ring-food.herokuapp.com/api/user/${id}`)
   }
+
   return (
     <div>
       <div className='row'>
@@ -26,8 +33,8 @@ const Login = () => {
             <div className='card Registion-card p-5'>
                 <form onSubmit={submit}>
                     <h3 className='text-center mb-5' >Login Form</h3>
-                    <input type="email" name='email' value={data.email} onChange={changeHandle} className='form-control mb-3 ' placeholder='Enter a Email id' />
-                    <input type="password" name='password' value={data.passwordpassword} onChange={changeHandle} className='form-control mb-3' placeholder='Enter a Password' />
+                    <input type="email" id='name' name='email' value={data.email} onChange={changeHandle} className='form-control mb-3 ' placeholder='Enter a Email id' />
+                    <input type="password" id='password' name='password' value={data.passwordpassword} onChange={changeHandle} className='form-control mb-3' placeholder='Enter a Password' />
                     <Link to=""><button className='btn  login-button'>Submit</button></Link> 
                     <Link to="/Registration"><button className='btn  login-newuser-button'>New User</button></Link>
 
